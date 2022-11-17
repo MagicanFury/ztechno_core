50b3e4182a4041de2158192f9838b9cd7734810c
Ivan Auda <ivan.auda@hva.nl>
2022-11-17 16:58:04 +0100

    Removed JQUERY

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,3 @@
-interface JQuery {
-  /**
-   * Converts JQuery Object to array of JQuery elements
-   * @returns {JQuery[]} Array of JQuery Elements
-   */
-  $arr: () => JQuery[];
-
-  /**
-   * Array map implementation with every element in the array representing a JQuery element
-   * @param {(value: JQuery, index: number, array: readonly JQuery[]) => U} callbackfn
-   * @returns {U[]} Collection of map return values
-   */
-  $map: <U>(callbackfn: (value: JQuery, index: number, array: readonly JQuery[]) => U) => U[];
-}
-
 type ZDom = {} & ZNode;
 
 type ZNode = {
