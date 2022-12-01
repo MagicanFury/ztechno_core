34ae9c0e0ae7ade4b39b0c283fdc58b05b6312f6
Ivan Auda <ivan.auda@hva.nl>
2022-12-01 03:35:58 +0100

    Added ZUserService to export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { ZCryptoService } from './crypto_service';
 import { ZMailService } from './mail_service';
 import { ZSqlService } from './sql_service';
 import { ZTranslateService } from './translate_service';
+import { ZUserService } from './user_service';
 
-export { ZCryptoService, ZMailService, ZSqlService, ZTranslateService };
+export { ZCryptoService, ZMailService, ZSqlService, ZTranslateService, ZUserService };
 // declare module 'ztechno_core' {
 // }
