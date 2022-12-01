3b78d9c6cddc72e9e30142420654cd6f93854897
Ivan Auda <ivan.auda@hva.nl>
2022-12-01 01:09:15 +0100

    Implemented mysql pool for ZSqlService

--- src/sql_service.ts
+++ src/sql_service.ts
@@ -1,34 +1,48 @@
 import * as mysql from 'mysql'
 
 let instance: ZSqlService | null = null
-const logError = (err: any) => { if (err) throw err }
+const handleError = (err: any) => { if (err) throw err }
 
 export class ZSqlService {
 
-  constructor(
-    private credentials: mysql.ConnectionConfig
-  ) {}
+  private pool: mysql.Pool
+  private defaultPoolconfig: mysql.PoolConfig = {
+    timeout: 20000,
+    connectTimeout: 10
+  }
 
-  connect(): Promise<mysql.Connection> {
-    return new Promise<any>((resolve, reject) => {
-      const con = mysql.createConnection(this.credentials);
-      con.connect((err: any) => {
-        if (err) return reject(err)
-        resolve(con)
+  constructor(options: mysql.PoolConfig) {
+    this.pool = mysql.createPool(Object.assign({}, this.defaultPoolconfig, options))
+    this.pool.on('connection', (connection: mysql.Connection) => {
+      connection.on('error', (err) => {
+        console.error(new Date(), 'MySQL error', err.code)
+      })
+      connection.on('close', (err) => {
+        console.error(new Date(), 'MySQL close', err)
       })
     })
   }
 
-  async query(sql: string, escaped: any[] = []): Promise<any> {
-    const con = await this.connect()
-    const output = await new Promise((resolve, reject) => {
-      con.query(sql, escaped, (err, result) => {
-        if (err) return reject(err)
-        resolve(result)
-      })
+  private getPoolConnection(): Promise<mysql.PoolConnection> {
+    return new Promise<any>((resolve, reject) => {
+      this.pool.getConnection((err: mysql.MysqlError, con: mysql.PoolConnection) => 
+        (err) ? reject(err) : resolve(con))
     })
-    con.end(logError)
-    return output
+  }
+
+  async query(sql: string, escaped: any[] = []): Promise<any> {
+    const con = await this.getPoolConnection()
+    try {
+      const output = await new Promise((resolve, reject) => {
+        con.query(sql, escaped, (err, result) =>
+          (err) ? reject(err) : resolve(result))
+      })
+      return output
+    } catch (err) {
+      handleError(err)
+    } finally {
+      con.release()
+    }
   }
 
   static get(credentials?: mysql.ConnectionConfig): ZSqlService {
