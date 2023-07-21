3c03427fc31d4b055be8de4018870745b08d753a
Ivan Auda <ivan.auda@hva.nl>
2023-07-21 03:20:18 +0200

    Changed name to email in db

--- src/user_service.ts
+++ src/user_service.ts
@@ -3,7 +3,7 @@ import { ZSqlService } from "./sql_service"
 
 type ZUser = {
   id: number
-  name: string
+  email: string
   session: string
   role: string|null
   admin: 0|1
@@ -12,13 +12,13 @@ type ZUser = {
 }
 
 type ZRequiredUserColumns = {
-  name: string
+  email: string
   role: string|null
   pass: string
   admin: 0|1
 }
 type ZUserCredentials = {
-  name: string
+  email: string
   pass: string
 }
 
@@ -58,7 +58,7 @@ export class ZUserService {
     await this.sqlService.query(`
       CREATE TABLE \`${this.tableName}\` (
         \`id\` int(10) unsigned NOT NULL AUTO_INCREMENT,
-        \`name\` varchar(64) NOT NULL,
+        \`email\` varchar(64) NOT NULL,
         \`role\` varchar(64) DEFAULT NULL,
         \`pass\` varchar(512) NOT NULL,
         \`session\` varchar(512) NOT NULL,
@@ -66,8 +66,8 @@ export class ZUserService {
         \`updated_at\` datetime NOT NULL DEFAULT current_timestamp(),
         \`created_at\` datetime NOT NULL DEFAULT current_timestamp(),
         PRIMARY KEY (\`id\`),
-        UNIQUE KEY \`name_UNIQUE\` (\`name\`),
-        KEY \`name\` (\`name\`),
+        UNIQUE KEY \`name_UNIQUE\` (\`email\`),
+        KEY \`email\` (\`email\`),
         KEY \`createdat\` (\`created_at\`),
         KEY \`updatedat\` (\`updated_at\`),
         KEY \`session\` (\`session\`)
@@ -82,12 +82,12 @@ export class ZUserService {
     }
   }
 
-  public async register({name, pass, role, admin}: ZRequiredUserColumns): Promise<{ session: string }> {
-    const session = this.genSession({ name, pass })
+  public async register({email, pass, role, admin}: ZRequiredUserColumns): Promise<{ session: string }> {
+    const session = this.genSession({ email, pass })
     await this.sqlService.query(`
-      INSERT INTO \`${this.tableName}\` (name, pass, session, role, admin)
+      INSERT INTO \`${this.tableName}\` (email, pass, session, role, admin)
       VALUES (?, ?, ?, ?, ?)
-    `, [name, this.hashPass({name, pass}), session, role, admin])
+    `, [email, this.hashPass({email, pass}), session, role, admin])
     return { session }
   }
 
@@ -101,7 +101,7 @@ export class ZUserService {
 
   public async auth(opt: ZUserSession|ZUserCredentials): Promise<{user?: ZUser, session?: string, authenticated: boolean}>
   public async auth(opt: Partial<ZUserSession & ZUserCredentials>): Promise<{user?: ZUser, session?: string, authenticated: boolean}> {
-    if (!opt.session && (!opt.name && !opt.pass)) {
+    if (!opt.session && (!opt.email && !opt.pass)) {
       return { authenticated: false }
     }
     const res = await ((opt.session) ? this.sqlService.query<ZUser[]>(`
@@ -109,18 +109,18 @@ export class ZUserService {
       WHERE session=?`, [opt.session]
     ) : this.sqlService.query<ZUser[]>(`
       SELECT id, name, session, role, admin, updated_at, created_at FROM \`${this.tableName}\`
-      WHERE name=? AND pass=?`, [opt.name, this.hashPass(opt as any)]
+      WHERE name=? AND pass=?`, [opt.email, this.hashPass(opt as any)]
     ))
     return (res.length === 0) ? { authenticated: false } : { user: res[0], session: res[0].session, authenticated: true }
   }
 
-  private genSession({ name }: ZUserCredentials) {
+  private genSession({ email: name }: ZUserCredentials) {
     const salt = this.salt
     const data = name + (Date.now() * Math.random())
     return ZCryptoService.hash('sha256', data, { saltMode: 'simple', salt })
   }
 
-  private hashPass({ name, pass }: ZUserCredentials): string {
+  private hashPass({ email, pass }: ZUserCredentials): string {
     const salt = name + this.salt
     return ZCryptoService.hash('sha256', pass, { saltMode: 'simple', salt })
   }
