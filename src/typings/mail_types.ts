a113924638ebcbbccf7da49edebdf6fee8d2564d
Ivan Auda <ivan.auda@hva.nl>
2024-01-21 21:14:21 +0100

    Added dkim to ZMailService constructor

--- src/typings/mail_types.ts
+++ src/typings/mail_types.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-export type MailServiceOptions = {auth: { user: string; pass: string }; mailSender: string}
+export type MailServiceOptions = {auth: { user: string; pass: string }; mailSender: string} & Partial<Pick<nodemailer.SendMailOptions, 'dkim'>>
 export type MailOptionsBase = {recipient: string, subject: string, from?: string} & Partial<Pick<nodemailer.SendMailOptions, 'dkim'|'priority'>>
 export type MailOptionsText = MailOptionsBase & {body: string}
 export type MailOptionsHtml = MailOptionsBase & {html: string}
