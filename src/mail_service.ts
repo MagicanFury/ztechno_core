a8bc7bf4ecb8715010824e24788f1ab11134c774
Ivan Auda <ivan.auda@hva.nl>
2024-01-21 21:00:58 +0100

    Updated ZMailService to support dkim

--- src/mail_service.ts
+++ src/mail_service.ts
@@ -1,35 +1,34 @@
+import SMTPTransport from "nodemailer/lib/smtp-transport"
 import { MailOptions, MailOptionsHtml, MailOptionsText, MailServiceOptions } from "./typings/mail_types"
 
-const nodemailer = require('nodemailer')
+import nodemailer from 'nodemailer'
 
 export class ZMailService {
 
   constructor(private opt: MailServiceOptions) {}
 
-  public send(mailOpts: MailOptionsText|MailOptionsHtml): Promise<any>
-  public send(mailOpts: MailOptions): Promise<any> {
+  public send(mailOpts: MailOptionsText|MailOptionsHtml): Promise<SMTPTransport.SentMessageInfo>
+  public send(mailOpts: MailOptions): Promise<SMTPTransport.SentMessageInfo> {
     const mailTransporter = nodemailer.createTransport({
       service: 'gmail',
       auth: this.opt.auth,
     })
 
-    const mailDetails = {
+    const mailDetails: nodemailer.SendMailOptions = {
       from: mailOpts.from || this.opt.mailSender,
       to: mailOpts.recipient,
       subject: mailOpts.subject,
       text: mailOpts.body || undefined,
-      html: mailOpts.html || undefined
+      html: mailOpts.html || undefined,
+      dkim: mailOpts.dkim,
+      priority: mailOpts.priority,
     }
 
     return new Promise((resolve, reject) => {
-      mailTransporter.sendMail(mailDetails, function (err: any, data: any) {
+      mailTransporter.sendMail(mailDetails, function (err: any, data: SMTPTransport.SentMessageInfo) {
         return err ? reject(err) : resolve(data)
       })
     })
   }
 
-  // protected static service: ZMailService
-  // static getOrCreate(opt: MailServiceOptions): ZMailService {
-  //   return (ZMailService.service == null) ? (ZMailService.service = new ZMailService(opt)) : ZMailService.service
-  // }
 }
--- src/typings/mail_types.ts
+++ src/typings/mail_types.ts
@@ -1,5 +1,7 @@
+import nodemailer from 'nodemailer'
+
 export type MailServiceOptions = {auth: { user: string; pass: string }; mailSender: string}
-export type MailOptionsBase = {recipient: string, subject: string, from?: string}
+export type MailOptionsBase = {recipient: string, subject: string, from?: string} & Pick<nodemailer.SendMailOptions, 'dkim'|'priority'>
 export type MailOptionsText = MailOptionsBase & {body: string}
 export type MailOptionsHtml = MailOptionsBase & {html: string}
 export type MailOptions = MailOptionsBase & {body?: string, html?: string}
