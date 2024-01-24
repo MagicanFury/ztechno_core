0d0af6ff17ca22f883b42d83d6dd8bdeab2aa2c3
Ivan Auda <ivan.auda@hva.nl>
2024-01-24 04:47:57 +0100

    Removed semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { ZEngineBase } from './engine_base';
-import { ZCryptoService } from './crypto_service';
-import { ZMailService } from './mail_service';
-import { ZSqlService } from './sql_service';
-import { ZTranslateService } from './translate_service';
-import { ZUserService } from './user_service';
+import { ZEngineBase } from './engine_base'
+import { ZCryptoService } from './crypto_service'
+import { ZMailService } from './mail_service'
+import { ZSqlService } from './sql_service'
+import { ZTranslateService } from './translate_service'
+import { ZUserService } from './user_service'
 
 export { HashStruct } from './typings/crypto_types'
-
 export { MailOptions, MailOptionsBase, MailOptionsHtml, MailOptionsText, MailServiceOptions, MailResponse } from './typings/mail_types'
 export { TranslateData, ZDom, ZNode, ZNodeText, dbTranslationRow } from './typings/translate_types'
 export { ZRequiredUserColumns, ZUser, ZUserCredentials, ZUserSession  } from './typings/user_types'
-
-export { ZCryptoService, ZMailService, ZSqlService, ZTranslateService, ZUserService, ZEngineBase };
+export { ZCryptoService, ZMailService, ZSqlService, ZTranslateService, ZUserService, ZEngineBase }
