fc0c6d4f738874256da08ba49c7ab1393fb20fef
Ivan Auda <ivan.auda@hva.nl>
2024-02-05 01:18:53 +0100

    Removed whitespace

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ import { ZUserService } from './user_service'
 export { HashStruct } from './typings/crypto_types'
 export { MailOptions, MailOptionsBase, MailOptionsHtml, MailOptionsText, MailServiceOptions, MailResponse } from './typings/mail_types'
 export { TranslateData, ZDom, ZNode, ZNodeText, dbTranslationRow } from './typings/translate_types'
-export { ZRequiredUserColumns, ZUser, ZUserCredentials, ZUserSession  } from './typings/user_types'
+export { ZRequiredUserColumns, ZUser, ZUserCredentials, ZUserSession } from './typings/user_types'
 export { ZCryptoService, ZMailService, ZSqlService, ZTranslateService, ZUserService, ZEngineBase }
