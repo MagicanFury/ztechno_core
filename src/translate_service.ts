4d2f08c34b3c6fea0bed838f00244079acec6db7
Ivan Auda <ivan.auda@hva.nl>
2025-07-24 04:28:48 +0200

    fix: Remove unused functions in translate_service.ts

--- src/translate_service.ts
+++ src/translate_service.ts
@@ -257,6 +257,216 @@ export class ZTranslateService {
     [`&#9792;`]: `♀`,
     [`&#9794;`]: `♂`,
     [`&#10084;`]: `❤`,
+    
+    // Arabic characters (U+0600 to U+06FF range)
+    [`&#1536;`]: `؀`,  // Arabic Number Sign
+    [`&#1537;`]: `؁`,  // Arabic Sign Sanah
+    [`&#1538;`]: `؂`,  // Arabic Sign Safha
+    [`&#1539;`]: `؃`,  // Arabic Sign Radi Allaahu Anhu
+    [`&#1540;`]: `؄`,  // Arabic Sign Radi Allaahu Anha
+    [`&#1541;`]: `؅`,  // Arabic Sign Radi Allaahu Anhum
+    [`&#1542;`]: `؆`,  // Arabic-Indic Cube Root
+    [`&#1543;`]: `؇`,  // Arabic-Indic Fourth Root
+    [`&#1544;`]: `؈`,  // Arabic Ray
+    [`&#1545;`]: `؉`,  // Arabic-Indic Per Mille Sign
+    [`&#1546;`]: `؊`,  // Arabic-Indic Per Ten Thousand Sign
+    [`&#1547;`]: `؋`,  // Afghani Sign
+    [`&#1548;`]: `،`,  // Arabic Comma
+    [`&#1549;`]: `؍`,  // Arabic Date Separator
+    [`&#1550;`]: `؎`,  // Arabic Poetic Verse Sign
+    [`&#1551;`]: `؏`,  // Arabic Sign Misra
+    [`&#1552;`]: `ؐ`,  // Arabic Sign Sallallahou Alayhe Wassallam
+    [`&#1553;`]: `ؑ`,  // Arabic Sign Alayhe Assallam
+    [`&#1554;`]: `ؒ`,  // Arabic Sign Rahmatullahi Alayhe
+    [`&#1555;`]: `ؓ`,  // Arabic Sign Radi Allaahu Anhu
+    [`&#1556;`]: `ؔ`,  // Arabic Sign Radi Allaahu Anha
+    [`&#1557;`]: `ؕ`,  // Arabic Sign Radi Allaahu Anhum
+    [`&#1558;`]: `ؖ`,  // Arabic Sign Radi Allaahu Anhunna
+    [`&#1559;`]: `ؗ`,  // Arabic Sign Radi Allaahu Anhumaa
+    [`&#1560;`]: `ؘ`,  // Arabic Sign Radi Allaahu Anhaa
+    [`&#1561;`]: `ؙ`,  // Arabic Sign Radi Allaahu Anhu
+    [`&#1562;`]: `ؚ`,  // Arabic Sign Radi Allaahu Anha
+    [`&#1563;`]: `؛`,  // Arabic Semicolon
+    [`&#1564;`]: `؜`,  // Arabic Letter Mark
+    [`&#1565;`]: `؝`,  // Arabic Triple Dot Punctuation Mark
+    [`&#1566;`]: `؞`,  // Arabic Start of Rub El Hizb
+    [`&#1567;`]: `؟`,  // Arabic Question Mark
+    [`&#1568;`]: `ؠ`,  // Arabic Letter Hamza
+    [`&#1569;`]: `ء`,  // Arabic Letter Alef with Hamza Above
+    [`&#1570;`]: `آ`,  // Arabic Letter Alef with Madda Above
+    [`&#1571;`]: `أ`,  // Arabic Letter Alef with Hamza Above
+    [`&#1572;`]: `ؤ`,  // Arabic Letter Waw with Hamza Above
+    [`&#1573;`]: `إ`,  // Arabic Letter Alef with Hamza Below
+    [`&#1574;`]: `ئ`,  // Arabic Letter Yeh with Hamza Above
+    [`&#1575;`]: `ا`,  // Arabic Letter Alef
+    [`&#1576;`]: `ب`,  // Arabic Letter Beh
+    [`&#1577;`]: `ة`,  // Arabic Letter Teh Marbuta
+    [`&#1578;`]: `ت`,  // Arabic Letter Teh
+    [`&#1579;`]: `ث`,  // Arabic Letter Theh
+    [`&#1580;`]: `ج`,  // Arabic Letter Jeem
+    [`&#1581;`]: `ح`,  // Arabic Letter Hah
+    [`&#1582;`]: `خ`,  // Arabic Letter Khah
+    [`&#1583;`]: `د`,  // Arabic Letter Dal
+    [`&#1584;`]: `ذ`,  // Arabic Letter Thal
+    [`&#1585;`]: `ر`,  // Arabic Letter Reh
+    [`&#1586;`]: `ز`,  // Arabic Letter Zain
+    [`&#1587;`]: `س`,  // Arabic Letter Seen
+    [`&#1588;`]: `ش`,  // Arabic Letter Sheen
+    [`&#1589;`]: `ص`,  // Arabic Letter Sad
+    [`&#1590;`]: `ض`,  // Arabic Letter Dad
+    [`&#1591;`]: `ط`,  // Arabic Letter Tah
+    [`&#1592;`]: `ظ`,  // Arabic Letter Zah
+    [`&#1593;`]: `ع`,  // Arabic Letter Ain
+    [`&#1594;`]: `غ`,  // Arabic Letter Ghain
+    [`&#1595;`]: `ػ`,  // Arabic Letter Keheh with Three Dots Above
+    [`&#1596;`]: `ؼ`,  // Arabic Letter Keheh with Three Dots Below
+    [`&#1597;`]: `ؽ`,  // Arabic Letter Farsi Yeh with Inverted V
+    [`&#1598;`]: `ؾ`,  // Arabic Letter Farsi Yeh with Two Dots Above
+    [`&#1599;`]: `ؿ`,  // Arabic Letter Farsi Yeh with Three Dots Above
+    [`&#1600;`]: `ـ`,  // Arabic Tatweel
+    [`&#1601;`]: `ف`,  // Arabic Letter Feh
+    [`&#1602;`]: `ق`,  // Arabic Letter Qaf
+    [`&#1603;`]: `ك`,  // Arabic Letter Kaf
+    [`&#1604;`]: `ل`,  // Arabic Letter Lam
+    [`&#1605;`]: `م`,  // Arabic Letter Meem
+    [`&#1606;`]: `ن`,  // Arabic Letter Noon
+    [`&#1607;`]: `ه`,  // Arabic Letter Heh
+    [`&#1608;`]: `و`,  // Arabic Letter Waw
+    [`&#1609;`]: `ى`,  // Arabic Letter Alef Maksura
+    [`&#1610;`]: `ي`,  // Arabic Letter Yeh
+    
+    // Arabic diacritics (most common ones)
+    [`&#1611;`]: `ً`,  // Arabic Fathatan
+    [`&#1612;`]: `ٌ`,  // Arabic Dammatan
+    [`&#1613;`]: `ٍ`,  // Arabic Kasratan
+    [`&#1614;`]: `َ`,  // Arabic Fatha
+    [`&#1615;`]: `ُ`,  // Arabic Damma
+    [`&#1616;`]: `ِ`,  // Arabic Kasra
+    [`&#1617;`]: `ّ`,  // Arabic Shadda
+    [`&#1618;`]: `ْ`,  // Arabic Sukun
+    [`&#1619;`]: `ٓ`,  // Arabic Maddah Above
+    [`&#1620;`]: `ٔ`,  // Arabic Hamza Above
+    [`&#1621;`]: `ٕ`,  // Arabic Hamza Below
+    
+    // Arabic-Indic digits
+    [`&#1632;`]: `٠`,  // Arabic-Indic Digit Zero
+    [`&#1633;`]: `١`,  // Arabic-Indic Digit One
+    [`&#1634;`]: `٢`,  // Arabic-Indic Digit Two
+    [`&#1635;`]: `٣`,  // Arabic-Indic Digit Three
+    [`&#1636;`]: `٤`,  // Arabic-Indic Digit Four
+    [`&#1637;`]: `٥`,  // Arabic-Indic Digit Five
+    [`&#1638;`]: `٦`,  // Arabic-Indic Digit Six
+    [`&#1639;`]: `٧`,  // Arabic-Indic Digit Seven
+    [`&#1640;`]: `٨`,  // Arabic-Indic Digit Eight
+    [`&#1641;`]: `٩`,  // Arabic-Indic Digit Nine
+    
+    // Extended Arabic characters (commonly used)
+    [`&#1642;`]: `٪`,  // Arabic Percent Sign
+    [`&#1643;`]: `٫`,  // Arabic Decimal Separator
+    [`&#1644;`]: `٬`,  // Arabic Thousands Separator
+    [`&#1645;`]: `٭`,  // Arabic Five Pointed Star
+    [`&#1646;`]: `ٮ`,  // Arabic Letter Dotless Beh
+    [`&#1647;`]: `ٯ`,  // Arabic Letter Dotless Qaf
+    [`&#1648;`]: `ٰ`,  // Arabic Letter Superscript Alef
+    [`&#1649;`]: `ٱ`,  // Arabic Letter Alef Wasla
+    [`&#1650;`]: `ٲ`,  // Arabic Letter Alef with Wavy Hamza Above
+    [`&#1651;`]: `ٳ`,  // Arabic Letter Alef with Wavy Hamza Below
+    [`&#1652;`]: `ٴ`,  // Arabic Letter High Hamza
+    [`&#1653;`]: `ٵ`,  // Arabic Letter High Hamza Alef
+    [`&#1654;`]: `ٶ`,  // Arabic Letter High Hamza Waw
+    [`&#1655;`]: `ٷ`,  // Arabic Letter U with Hamza Above
+    [`&#1656;`]: `ٸ`,  // Arabic Letter High Hamza Yeh
+    [`&#1657;`]: `ٹ`,  // Arabic Letter Tteh
+    [`&#1658;`]: `ٺ`,  // Arabic Letter Tteheh
+    [`&#1659;`]: `ٻ`,  // Arabic Letter Beeh
+    [`&#1660;`]: `ټ`,  // Arabic Letter Teh with Ring
+    [`&#1661;`]: `ٽ`,  // Arabic Letter Teh with Three Dots Above Downwards
+    [`&#1662;`]: `پ`,  // Arabic Letter Peh
+    [`&#1663;`]: `ٿ`,  // Arabic Letter Teheh
+    [`&#1664;`]: `ڀ`,  // Arabic Letter Beheh
+    [`&#1665;`]: `ځ`,  // Arabic Letter Hah with Hamza Above
+    [`&#1666;`]: `ڂ`,  // Arabic Letter Hah with Two Dots Vertical Above
+    [`&#1667;`]: `ڃ`,  // Arabic Letter Nyeh
+    [`&#1668;`]: `ڄ`,  // Arabic Letter Dyeh
+    [`&#1669;`]: `څ`,  // Arabic Letter Hah with Three Dots Above
+    [`&#1670;`]: `چ`,  // Arabic Letter Tcheh
+    [`&#1671;`]: `ڇ`,  // Arabic Letter Tcheheh
+    [`&#1672;`]: `ڈ`,  // Arabic Letter Ddal
+    [`&#1673;`]: `ډ`,  // Arabic Letter Dal with Ring
+    [`&#1674;`]: `ڊ`,  // Arabic Letter Dal with Dot Below
+    [`&#1675;`]: `ڋ`,  // Arabic Letter Dal with Dot Below and Small Tah
+    [`&#1676;`]: `ڌ`,  // Arabic Letter Dahal
+    [`&#1677;`]: `ڍ`,  // Arabic Letter Ddahal
+    [`&#1678;`]: `ڎ`,  // Arabic Letter Dul
+    [`&#1679;`]: `ڏ`,  // Arabic Letter Dal with Three Dots Above Downwards
+    [`&#1680;`]: `ڐ`,  // Arabic Letter Dal with Four Dots Above
+    [`&#1681;`]: `ڑ`,  // Arabic Letter Rreh
+    [`&#1682;`]: `ڒ`,  // Arabic Letter Reh with Small V
+    [`&#1683;`]: `ړ`,  // Arabic Letter Reh with Ring
+    [`&#1684;`]: `ڔ`,  // Arabic Letter Reh with Dot Below
+    [`&#1685;`]: `ڕ`,  // Arabic Letter Reh with Small V Below
+    [`&#1686;`]: `ږ`,  // Arabic Letter Reh with Dot Below and Dot Above
+    [`&#1687;`]: `ڗ`,  // Arabic Letter Reh with Two Dots Above
+    [`&#1688;`]: `ژ`,  // Arabic Letter Jeh
+    [`&#1689;`]: `ڙ`,  // Arabic Letter Reh with Four Dots Above
+    [`&#1690;`]: `ښ`,  // Arabic Letter Seen with Dot Below and Dot Above
+    [`&#1691;`]: `ڛ`,  // Arabic Letter Seen with Three Dots Below
+    [`&#1692;`]: `ڜ`,  // Arabic Letter Seen with Three Dots Below and Three Dots Above
+    [`&#1693;`]: `ڝ`,  // Arabic Letter Arab
+    [`&#1694;`]: `ڞ`,  // Arabic Letter Seen with Two Dots Vertical Above
+    [`&#1695;`]: `ڟ`,  // Arabic Letter Seen with Inverted V
+    [`&#1696;`]: `ڠ`,  // Arabic Letter Seen with Two Dots Above
+    [`&#1697;`]: `ڡ`,  // Arabic Letter Dotless Feh
+    [`&#1698;`]: `ڢ`,  // Arabic Letter Feh with Dot Moved Below
+    [`&#1699;`]: `ڣ`,  // Arabic Letter Feh with Dot Below
+    [`&#1700;`]: `ڤ`,  // Arabic Letter Veh
+    [`&#1701;`]: `ڥ`,  // Arabic Letter Feh with Three Dots Below
+    [`&#1702;`]: `ڦ`,  // Arabic Letter Peheh
+    [`&#1703;`]: `ڧ`,  // Arabic Letter Qaf with Dot Above
+    [`&#1704;`]: `ڨ`,  // Arabic Letter Qaf with Three Dots Above
+    [`&#1705;`]: `ک`,  // Arabic Letter Keheh
+    [`&#1706;`]: `ڪ`,  // Arabic Letter Swash Kaf
+    [`&#1707;`]: `ګ`,  // Arabic Letter Kaf with Ring
+    [`&#1708;`]: `ڬ`,  // Arabic Letter Kaf with Dot Above
+    [`&#1709;`]: `ڭ`,  // Arabic Letter Ng
+    [`&#1710;`]: `ڮ`,  // Arabic Letter Kaf with Three Dots Below
+    [`&#1711;`]: `گ`,  // Arabic Letter Gaf
+    [`&#1712;`]: `ڰ`,  // Arabic Letter Gaf with Ring
+    [`&#1713;`]: `ڱ`,  // Arabic Letter Ngoeh
+    [`&#1714;`]: `ڲ`,  // Arabic Letter Gaf with Two Dots Below
+    [`&#1715;`]: `ڳ`,  // Arabic Letter Gueh
+    [`&#1716;`]: `ڴ`,  // Arabic Letter Gaf with Three Dots Above
+    [`&#1717;`]: `ڵ`,  // Arabic Letter Lam with Small V
+    [`&#1718;`]: `ڶ`,  // Arabic Letter Lam with Dot Above
+    [`&#1719;`]: `ڷ`,  // Arabic Letter Lam with Three Dots Above
+    [`&#1720;`]: `ڸ`,  // Arabic Letter Lam with Three Dots Below
+    [`&#1721;`]: `ڹ`,  // Arabic Letter Noon with Dot Below
+    [`&#1722;`]: `ں`,  // Arabic Letter Noon Ghunna
+    [`&#1723;`]: `ڻ`,  // Arabic Letter Rnoon
+    [`&#1724;`]: `ڼ`,  // Arabic Letter Noon with Ring
+    [`&#1725;`]: `ڽ`,  // Arabic Letter Noon with Three Dots Above
+    [`&#1726;`]: `ھ`,  // Arabic Letter Heh Doachashmee
+    [`&#1727;`]: `ڿ`,  // Arabic Letter Tcheh with Dot Above
+    [`&#1728;`]: `ۀ`,  // Arabic Letter Heh with Yeh Above
+    [`&#1729;`]: `ہ`,  // Arabic Letter Heh Goal
+    [`&#1730;`]: `ۂ`,  // Arabic Letter Heh Goal with Hamza Above
+    [`&#1731;`]: `ۃ`,  // Arabic Letter Teh Marbuta Goal
+    [`&#1732;`]: `ۄ`,  // Arabic Letter Waw with Ring
+    [`&#1733;`]: `ۅ`,  // Arabic Letter Kirghiz Oe
+    [`&#1734;`]: `ۆ`,  // Arabic Letter Oe
+    [`&#1735;`]: `ۇ`,  // Arabic Letter U
+    [`&#1736;`]: `ۈ`,  // Arabic Letter Yu
+    [`&#1737;`]: `ۉ`,  // Arabic Letter Kirghiz Yu
+    [`&#1738;`]: `ۊ`,  // Arabic Letter Waw with Two Dots Above
+    [`&#1739;`]: `ۋ`,  // Arabic Letter Ve
+    [`&#1740;`]: `ی`,  // Arabic Letter Farsi Yeh
+    [`&#1741;`]: `ۍ`,  // Arabic Letter Yeh with Tail
+    [`&#1742;`]: `ێ`,  // Arabic Letter Yeh with Small V
+    [`&#1743;`]: `ۏ`,  // Arabic Letter Waw with Dot Above
+    [`&#1744;`]: `ې`,  // Arabic Letter E
+    [`&#1745;`]: `ۑ`,  // Arabic Letter Yeh with Three Dots Below
+    [`&#1746;`]: `ے`,  // Arabic Letter Yeh Barree
+    [`&#1747;`]: `ۓ`,  // Arabic Letter Yeh Barree with Hamza Above
   }
 
   public getLang(cookies: { [key: string]: string }): string {
@@ -629,7 +839,7 @@ export class ZTranslateService {
       }
 
       const results = await this.retryOperation(async () => {
-        return await this.sql.query<any>(`SELECT \`value\` FROM translations WHERE \`lang\`=? AND \`key\`=CONVERT(? USING utf8mb3)`, [lang, key])
+        return await this.sql.query<any>(`SELECT \`value\` FROM translations WHERE \`lang\`=? AND \`key\`=CONVERT(? USING utf8mb4)`, [lang, key])
       }, 'fetch translation')
 
       if (results.length > 0) {
@@ -720,7 +930,8 @@ export class ZTranslateService {
 }
 
 function strContainsLetters(text: string): boolean {
-  const regExp = /[a-zA-Z]/g
+  // Updated regex to include Arabic characters (U+0600-U+06FF) and other scripts
+  const regExp = /[a-zA-Z\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]/g
   return regExp.test(text)
 }
 
